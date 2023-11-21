7357bc23e930cde8418dd9e7103740aae686a080
oEg8 <136712609+oEg8@users.noreply.github.com>
2023-11-21 14:25:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
 VERSION = '0.0.1'
-DESCRIPTION = ''
-LONG_DESCRITION = ''
+DESCRIPTION = 'Tutorial voor de Hogeschool Rotterdam'
+LONG_DESCRIPTION = 'Een library gemaakt voor de les in week 47.'
 
+# Setting up
 setup(
-    name='',
+    name="hrfmp",
     version=VERSION,
-    author='',
-    author_email='',
+    author="Bart Westhoff",
+    author_email="0991807@hr.nl",
     description=DESCRIPTION,
-    long_description=LONG_DESCRITION,
-    packages='',
-    install_requirements='',
-    keywords=[''],
-    classifiers=[''],
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas'],
+    keywords=['python', 'school', 'financial sector'],
+    classifiers=[
+        "Development Status :: 3 - Release",
+        "Intended Audience :: Students",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
